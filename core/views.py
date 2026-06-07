310e19e54aa79e41028910fbf5aab8e00f576fb2
Sergey Korobeynikov <skoro@smn35.com>
2026-06-07 03:42:03 +0300

    "corsheaders",     "django_filters",     "drf_yasg" added to INSTALLED_APPS
    CORS_ALLOW_ALL_ORIGINS = True
    filter settings fix in core/view.py

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Items
-from serializers import ItemsSerializer
+from .serializers import ItemsSerializer
+
 
 class ItemsViewSet(viewsets.ModelViewSet):
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    ordering_fields = ['name', 'quantity', 'distance']
+    filterset_fields = ['date', 'name', 'quantity', 'distance']
