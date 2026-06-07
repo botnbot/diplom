c4b9c7cba0ab9492d87f3e5fabcabf899ffc245d
Sergey Korobeynikov <skoro@smn35.com>
2026-06-07 13:49:15 +0300

    class descriptions added

--- core/views.py
+++ core/views.py
@@ -1,14 +1,18 @@
-from rest_framework import viewsets
-from rest_framework import viewsets
-from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
+
 from .models import Items
 from .serializers import ItemsSerializer
 
 
 class ItemsViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с Items.
+    Поддерживает CRUD операции, фильтрацию, сортировку и пагинацию.
+    """
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
-    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering_fields = ['name', 'quantity', 'distance']
     filterset_fields = ['date', 'name', 'quantity', 'distance']
