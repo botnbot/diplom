dabf214227b4cab67ede0cfa6a19e171be8ec38e
Sergey Korobeynikov <skoro@smn35.com>
2026-06-07 17:50:54 +0300

    urlpattern to '' for index.html added

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views_frontend import index
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -11,14 +12,14 @@ schema_view = get_schema_view(
         description="API для управления маршрутами",
     ),
     public=True,
-    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
-
 ]
