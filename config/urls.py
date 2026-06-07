effe26b15228c82ab82efda8bd9d3815126b4de7
Sergey Korobeynikov <skoro@smn35.com>
2026-06-07 15:13:05 +0300

    set permissions to Swager  urlpatterns to swagger & redoc added

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DyplomProject API",
+        default_version='v1',
+        description="API для управления маршрутами",
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+
 ]
