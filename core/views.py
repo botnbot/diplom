76ed0d45aeb282c19ed8ffa9ea6b6d359c3ac0d9
Sergey Korobeynikov <skoro@smn35.com>
2026-06-07 17:53:27 +0300

    filterset_fields in class ItemsViewSet changed

--- core/views.py
+++ core/views.py
@@ -1,18 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
-
 from .models import Items
 from .serializers import ItemsSerializer
 
 
 class ItemsViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet для работы с Items.
-    Поддерживает CRUD операции, фильтрацию, сортировку и пагинацию.
-    """
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = {
+        'name': ['exact', 'contains', 'icontains'],
+        'quantity': ['exact', 'gte', 'lte'],
+        'distance': ['exact', 'gte', 'lte'],
+        'date': ['exact', 'gte', 'lte'],
+    }
     ordering_fields = ['name', 'quantity', 'distance']
-    filterset_fields = ['date', 'name', 'quantity', 'distance']
