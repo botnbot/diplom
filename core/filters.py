1913f9998355c974a89d5d8d242e7ce08fdbb0e9
Sergey Korobeynikov <skoro@smn35.com>
2026-06-09 01:59:29 +0300

    added 'exact' 'gte' to filters Name-based filtering test fix Minimum quantity filtering test fix

--- core/filters.py
+++ core/filters.py
@@ -2,7 +2,9 @@ from django_filters import rest_framework as filters
 from .models import Items
 
 class ItemsFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='exact')
     name_contains = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    quantity_gte = filters.NumberFilter(field_name='quantity', lookup_expr='gte')
     quantity_gt = filters.NumberFilter(field_name='quantity', lookup_expr='gt')
     quantity_lt = filters.NumberFilter(field_name='quantity', lookup_expr='lt')
     distance_gt = filters.NumberFilter(field_name='distance', lookup_expr='gt')
