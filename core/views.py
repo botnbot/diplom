1debd2bd3ae5bf14645ea309bcf880eb044126ec
Sergey Korobeynikov <skoro@smn35.com>
2026-06-09 00:17:52 +0300

    filterset_class defined

--- core/views.py
+++ core/views.py
@@ -3,16 +3,11 @@ from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
 from .models import Items
 from .serializers import ItemsSerializer
-
+from .filters import ItemsFilter   # важно!
 
 class ItemsViewSet(viewsets.ModelViewSet):
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = {
-        'name': ['exact', 'contains', 'icontains'],
-        'quantity': ['exact', 'gte', 'lte'],
-        'distance': ['exact', 'gte', 'lte'],
-        'date': ['exact', 'gte', 'lte'],
-    }
+    filterset_class = ItemsFilter
     ordering_fields = ['name', 'quantity', 'distance']
