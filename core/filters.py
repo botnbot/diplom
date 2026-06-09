669398c6a4526737fcd7f79a07223afd4a2044b9
Sergey Korobeynikov <skoro@smn35.com>
2026-06-09 19:06:05 +0300

    distance_exact, quantity_exact,  name_exact  attributes to class ItemsFilter created

--- core/filters.py
+++ core/filters.py
@@ -2,11 +2,12 @@ from django_filters import rest_framework as filters
 from .models import Items
 
 class ItemsFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', lookup_expr='exact')
+    name_exact = filters.CharFilter(field_name='name', lookup_expr='exact')
     name_contains = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    quantity_gte = filters.NumberFilter(field_name='quantity', lookup_expr='gte')
+    quantity_exact = filters.NumberFilter(field_name='quantity', lookup_expr='exact')
     quantity_gt = filters.NumberFilter(field_name='quantity', lookup_expr='gt')
     quantity_lt = filters.NumberFilter(field_name='quantity', lookup_expr='lt')
+    distance_exact = filters.NumberFilter(field_name='distance', lookup_expr='exact')
     distance_gt = filters.NumberFilter(field_name='distance', lookup_expr='gt')
     distance_lt = filters.NumberFilter(field_name='distance', lookup_expr='lt')
 
