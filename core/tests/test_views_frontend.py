9e369afcf072a02a48a42599ea7b7d55458baac8
Sergey Korobeynikov <skoro@smn35.com>
2026-06-26 13:03:27 +0300

    some tests recovered

--- core/tests/test_views_frontend.py
+++ core/tests/test_views_frontend.py
@@ -1,17 +1,23 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 from core.models import Items
 
 class FrontendViewsTest(TestCase):
     def setUp(self):
-        self.item = Items.objects.create(name="Test Item", quantity=10)
+        self.item = Items.objects.create(
+            name="Test Item",
+            quantity=10,
+            date=timezone.now().date(),
+            distance=0
+        )
 
-    def test_home_page(self):
-        response = self.client.get(reverse('home'))
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/index.html')
 
-    def test_item_list_page(self):
-        response = self.client.get(reverse('item_list'))
+    def test_api_items_list(self):
+        response = self.client.get('/api/items/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Test Item")
+        self.assertContains(response, "Расстояние")
