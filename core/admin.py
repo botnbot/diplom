ed56c77ac66c97f95c4ab2dce87c230aa850a6a6
Sergey Korobeynikov <skoro@smn35.com>
2026-06-29 19:31:05 +0300

    core/admin.py created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Items
 
-# Register your models here.
+@admin.register(Items)
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'quantity', 'date', 'distance')
+    list_filter = ('name', 'quantity', 'date', 'distance')
+    search_fields = ('name', )
+    list_editable = ('name', 'quantity', 'distance')
+    fields = ('id', 'date', 'name', 'quantity', 'distance')
+    ordering = ('-date', )
+    list_per_page = 10
