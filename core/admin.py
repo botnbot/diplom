7efd353764fd39d2651ec2433f3a3916af4c49bb
Sergey Korobeynikov <skoro@smn35.com>
2026-07-04 21:13:08 +0300

    quantity&distance fields deletted from admin.py id field removed from fields

--- core/admin.py
+++ core/admin.py
@@ -4,9 +4,9 @@ from .models import Items
 @admin.register(Items)
 class ItemsAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'quantity', 'date', 'distance')
-    list_filter = ('name', 'quantity', 'date', 'distance')
-    search_fields = ('name', )
+    list_filter = ('name', 'date')
+    search_fields = ('name',)
     list_editable = ('name', 'quantity', 'distance')
-    fields = ('id', 'date', 'name', 'quantity', 'distance')
-    ordering = ('-date', )
+    fields = ('date', 'name', 'quantity', 'distance')  # ← id УБРАТЬ!
+    ordering = ('-date',)
     list_per_page = 10
