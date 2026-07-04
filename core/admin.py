868f16d74fd8f03b1774127d49e7f1d485635253
Sergey Korobeynikov <skoro@smn35.com>
2026-07-04 21:16:53 +0300

    quantity&distance fields deletted from admin.py

--- core/admin.py
+++ core/admin.py
@@ -7,6 +7,6 @@ class ItemsAdmin(admin.ModelAdmin):
     list_filter = ('name', 'date')
     search_fields = ('name',)
     list_editable = ('name', 'quantity', 'distance')
-    fields = ('date', 'name', 'quantity', 'distance')  # ← id УБРАТЬ!
+    fields = ('date', 'name', 'quantity', 'distance')
     ordering = ('-date',)
     list_per_page = 10
